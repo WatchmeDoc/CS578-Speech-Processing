c9615ac34fef8615dc287b90c91243f21ece1b94
George Manos <watchmedoctor@gmail.com>
2022-10-29 16:41:04 +0300

    added multiple files support

--- Lab0-Time-Domain-Speech/zero_crossings.m
+++ Lab0-Time-Domain-Speech/zero_crossings.m
@@ -1,66 +1,75 @@
-clear;
+clear; close all;
+
 frame_rate = 0.01;
 frame_length = 0.03;
 
-% Reading the speech signal
-[s, fs] = audioread('H.22.16k.wav');
+myDir = uigetdir; %gets directory
+myFiles = dir(fullfile(myDir,'*.wav')); %gets all wav files in struct
+for k = 1:length(myFiles)
+    baseFileName = myFiles(k).name;
+    fullFileName = fullfile(myDir, baseFileName);
+    fprintf(1, 'Now reading %s\n', fullFileName);
 
-% Remove mean value (DC component)
-s = s - mean(s);
+    % Reading the speech signal
+    [s, fs] = audioread(fullFileName);
 
-% Signal length
-D = length(s);
+    % Remove mean value (DC component)
+    s = s - mean(s);
 
-% Frame length (30 ms, how many samples? )
-L = fs * frame_length;
+    % Signal length
+    D = length(s);
 
-% Frame shift (10 ms, how many samples? )
-U = fs * frame_rate;
+    % Frame length (30 ms, how many samples? )
+    L = fs * frame_length;
 
-% Window type (Hamming)
-win = hamming(L);
+    % Frame shift (10 ms, how many samples? )
+    U = fs * frame_rate;
 
-% Number of frames
-Nfr = floor((D - L) / U) + 1;
+    % Window type (Hamming)
+    win = hamming(L);
 
-% Memory allocation (for speed)
-energy = zeros(1, Nfr+1);
-ZCr = zeros(1, Nfr+1);
+    % Number of frames
+    Nfr = floor((D - L) / U) + 1;
 
-% Loop which calculates the speech features
-for i = 1:1:Nfr
-    frame = s((i-1) * U + 1: (i-1) * U + L ) .* win; % a frame of speech windowed by the Hamming window
-    energy(i) = 1 / L * sum(frame.^2); % calculate energy
-    ZCr(i) =  1 / 2 * sum(abs(sign(frame(2:end)) - sign(frame(1:end-1)))); % calculate zero crossings
-    T(i) = L/2 + (i-1)*U; % Next analysis time instant
-end
+    % Memory allocation (for speed)
+    energy = zeros(1, Nfr+1);
+    ZCr = zeros(1, Nfr+1);
 
-% THRESHOLDS (you can play with it!)
-Ethres = mean(energy)/2;
-ZCRthres = (3/2)*mean(ZCr) - 0.3*std(ZCr);
-
-% Classification for each frame
-for i = 1:1:Nfr
-    if energy(i) > Ethres
-        % VOICED
-        VUS(i) = 1.0;
-    elseif ZCr(i) < ZCRthres
-        % SILENCE
-        VUS(i) = 0.0;
-    elseif ZCr(i) > ZCRthres
-        % UNVOICED
-        VUS(i) = 0.5;
+    % Loop which calculates the speech features
+    for i = 1:1:Nfr
+        frame = s((i-1) * U + 1: (i-1) * U + L ) .* win; % a frame of speech windowed by the Hamming window
+        energy(i) = 1 / L * sum(frame.^2); % calculate energy
+        ZCr(i) =  1 / 2 * sum(abs(sign(frame(2:end)) - sign(frame(1:end-1)))); % calculate zero crossings
+        T(i) = L/2 + (i-1)*U; % Next analysis time instant
     end
-end
-% Interpolation with interp1
-VUSi = interp1(T, VUS, 1:1:D);
 
-% Visualize
-figure;
-t = 0:1/fs:length(s)/fs-1/fs;
-plot(t, VUSi,'LineWidth', 2.5);
-hold on; plot(t, s/max(s), 'r'); hold off;
-xlabel('Time (s)');
-ylim([-2 2]);
-title('Energy & Zero-Crossings Rate-based VUS discrimination');
-grid;
+    % THRESHOLDS (you can play with it!)
+    Ethres = mean(energy)/2;
+    ZCRthres = (3/2)*mean(ZCr) - 0.3*std(ZCr);
+
+    % Classification for each frame
+    for i = 1:1:Nfr
+        if energy(i) > Ethres
+            % VOICED
+            VUS(i) = 1.0;
+        elseif ZCr(i) < ZCRthres
+            % SILENCE
+            VUS(i) = 0.0;
+        elseif ZCr(i) > ZCRthres
+            % UNVOICED
+            VUS(i) = 0.5;
+        end
+    end
+    % Interpolation with interp1
+    VUSi = interp1(T, VUS, 1:1:D);
+
+    % Visualize
+    figure;
+    t = 0:1/fs:length(s)/fs-1/fs;
+    plot(t, VUSi,'LineWidth', 2.5);
+    hold on; plot(t, s/max(s), 'r'); hold off;
+    xlabel('Time (s)');
+    ylim([-2 2]);
+    title('Energy & Zero-Crossings Rate-based VUS discrimination');
+    grid;
+end
