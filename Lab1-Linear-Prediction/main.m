01dae5f0951f26011d5dd185ecb8161694b17116
Alexandros Angelakis <angelakis@ics.forth.gr>
2022-12-21 17:05:44 +0200

    robot and whiper voice

--- Lab1-Linear-Prediction/main.m
+++ Lab1-Linear-Prediction/main.m
@@ -1,3 +1,4 @@
+%% normal
 file = 'speechsample.wav';
 out = lpc_as_toyou(file);
 [sig, fs] = audioread(file);
@@ -7,3 +8,23 @@ soundsc(out, fs);
 %sound(sig,fs);
 % sound([out [zeros(2000,1);sig(1:length(sig)-2000)]],fs); % create echo
 
+%% whisper
+file = 'speechsample.wav';
+out = whisper_exp(file);
+[sig, fs] = audioread(file);
+
+soundsc(out, fs);
+%sound(out,fs);
+%sound(sig,fs);
+% sound([out [zeros(2000,1);sig(1:length(sig)-2000)]],fs); % create echo
+
+
+%% robot
+file = 'speechsample.wav';
+out = robot_exp(file);
+[sig, fs] = audioread(file);
+
+soundsc(out, fs);
+%sound(out,fs);
+%sound(sig,fs);
+% sound([out [zeros(2000,1);sig(1:length(sig)-2000)]],fs); % create echo
---
+++ Lab1-Linear-Prediction/robot_exp.m
@@ -0,0 +1,64 @@
+function out = robot_exp(file)
+%
+% INPUT:
+%   file: input filename of a wav file
+% OUTPUT:
+%   out: a vector contaning the output signal
+%
+% Example:
+%   
+%   out = lpc_as('speechsample.wav');
+%   [sig,fs]= wavread('speechsample.wav');
+%   sound(out,fs);
+%   sound(sig,fs);
+%   sound([out [zeros(2000,1);sig(1:length(sig)-2000)]],fs); % create echo
+%
+%
+% Yannis Stylianou
+% CSD - CS 578
+%
+
+[sig, Fs] = audioread(file);
+
+Horizon = 30;  %30ms - window length
+OrderLPC=24;   %order of LPC
+Buffer = 0;    % initialization
+out = zeros(size(sig)); % initialization
+
+Horizon = Horizon*Fs/1000;
+Shift = Horizon/2;       % frame size - step size
+Win = hanning(Horizon);  % analysis window
+
+Lsig = length(sig);
+slice = 1:Horizon;
+tosave = 1:Shift;
+Nfr = floor((Lsig-Horizon)/Shift)+1;  % number of frames
+
+% analysis frame-by-frame
+for l=1:Nfr
+    
+  sigLPC = Win.*sig(slice);
+  en = sum(sigLPC.^2); % get the short - term energy of the input
+  
+  % LPC analysis
+  [r,lg] =  xcorr(sigLPC); % correlation
+  r = r(lg>=0);
+  a =  my_levinson(r,OrderLPC);  % LPC coef.
+  G =  sqrt(sum(a .* r(1:OrderLPC + 1).'));  % gain
+  ex = repelem([1 0], size(sigLPC,1)/2);
+  
+  % synthesis
+  s = filter(G,a, ex);
+  ens = sum(s.^2);   % get the short-time energy of the output
+  g = sqrt(en/ens);  % normalization factor
+  s  =s*g;           % energy compensation
+  s(1:Shift) = s(1:Shift) + Buffer;  % Overlap and add
+  out(tosave) = s(1:Shift);           % save the first part of the frame
+  Buffer = s(Shift+1:Horizon);       % buffer the rest of the frame
+  
+  slice = slice+Shift;   % move the frame
+  tosave = tosave+Shift;
+  
+end
+
+
