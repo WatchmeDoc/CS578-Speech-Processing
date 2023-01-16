1a1795bcf7b57d2b0bf714262a45d6f14c0f41e0
George Manos <watchmedoctor@gmail.com>
2023-01-16 21:45:59 +0200

    added lab4 files

---
+++ Lab4-Speech-Enhancement/SE.m
@@ -0,0 +1,31 @@
+% code to generate the noisy signal
+
+% read the file: it has 1000 zero samples in the beginning
+[s,fs]=wavread('furelise-1000z.wav');
+
+% listen to the file
+soundsc(s,fs); % nice ee?
+
+% generate noise; gaussian - zero mean, variance is one
+Noise = randn(size(s));
+
+% set the (global) SNR in dB
+SNR=10;
+
+% find the multiplicative coeff for the noise
+Es = sum(s.^2);
+En = sum(Noise.^2);
+a = sqrt(10^(-SNR/20)*Es/En);
+
+sn = s+a*Noise;
+
+% listen to the noisy file
+soundsc(sn,fs); % bad ee?
+
+% normalize it in order to save it as wav
+sn = sn/(1.1*max(abs(sn)));
+
+% save it for your work:
+wavwrite(sn,fs,16,'furelise-1000z-noise.wav');
+
+% so, clean it with Spectral subtraction and Wiener filtering.
