c0ba3db8337ad2885a6520260f264f2cbff1c11e
George Manos <watchmedoctor@gmail.com>
2023-02-08 01:26:49 +0200

    added lab2 report and personal files

--- Lab2-Sinusoidal-Modeling/main.m
+++ Lab2-Sinusoidal-Modeling/main.m
@@ -1,16 +1,18 @@
 clear; close all;
 
 [s, fs] = audioread('arctic_bdl1_snd_norm.wav');
-%soundsc(s, fs);
-%pause(4);
+soundsc(s, fs);
+pause(4);
 
 [Y, SNR] = SinM_test_hy578(s, fs);
 
 soundsc(Y, fs);
-fprintf('SNR: %f\n', SNR);
 y_padded = [Y; zeros(length(s) - length(Y), 1)];
+figure;
 subplot(211); plot(s); title('Original signal');
 subplot(212); plot(y_padded); title('Synthesized signal');
 
 MSE = (1/size(s,1)) * sum((s - y_padded).^2);
 fprintf('MSE: %f\n', MSE);
+figure;
+plot(SNR); title('SNR per frame'); xlabel('frame number'); ylabel('SNR (DB)');
